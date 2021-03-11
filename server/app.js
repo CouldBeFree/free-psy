133c96d26bca8f947cfb4f7c6f02c0b22b65536a
couldbefree <zoriaalexandr@gmail.com>
2021-03-11 07:45:18 +0200

    setuped server

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/uploads', express.static('uploads'));
+
+mongoose.connect('mongodb://localhost:27017/psy', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected')
+});
+
+const port = 5050;
+const server = app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+});
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
