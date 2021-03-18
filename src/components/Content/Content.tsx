6474e54ce43aa91c0c129473435de99a72c504ec
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-03-18 16:18:08 +0200

    implemented responsive design

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router';
 import style from './Content.module.css';
-import Main from '../Main/Main';
+import Main from './Main/Main';
+import Registration from './Registration/Registration';
 
 const Content = () => {
   return (
     <div className={style.content}>
-      <Main />
+      <Route exact path="/" render={() => <Main />} />
+      <Route path="/registration" render={() => <Registration />} />
     </div>
   )
 }
---
+++ src/components/Content/Main/Slider/Slider.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import style from './Slider.module.css';
+import Button from '../../../common/Button/Button';
+import Card from '../../../Card/Card';
+const Slider = () => {
+  return (
+    <div className={style.sliderBlock}>
+      <div className={style.wrapper}>
+        <h3 className={style.title}>Наші волонтери</h3>
+        <div className={style.slider}>
+          <div className={style.arrow}>
+            <Button>&#60;</Button>
+          </div>
+          <div className={style.cardBlock}>
+            <Card fullName={undefined} photoUrl={undefined} shortInfo={undefined} />
+          </div>
+          <div className={style.arrow}>
+            <Button>&#62;</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Slider;
