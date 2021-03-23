a9017feb5413f0a655a4cf6d7c96497830dd11c8
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-03-23 23:44:57 +0200

    finished 'form validation'

--- src/components/Content/Registration/LoginForm/LoginForm.tsx
+++ src/components/Content/Registration/LoginForm/LoginForm.tsx
@@ -1,46 +1,75 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '../../../common/Button/Button';
 import { Form, Field } from 'react-final-form';
 import style from './LoginForm.module.css';
+import { required, minLength, maxLength, repeatPassword, composeValidators } from '../../../../utilities/validators/general';
+import { passwordPattern, emailPattern } from '../../../../utilities/validators/pattern';
 
 const LoginForm = () => {
-
-  const [psychologistForm, setPsychologistForm] = useState(false);
-
-  const onChangeForm = () => {
-    setPsychologistForm(!psychologistForm);
-  }
 
   const onSubmit = (formData: any) => {
     console.log(formData);
   }
 
   return (
-    <Form onSubmit={onSubmit} initialValues={{ isPsychologist: [psychologistForm && true] }} render={({ handleSubmit }) => (
+    <Form onSubmit={onSubmit} render={({ handleSubmit, values }) => (
       <form className={style.form} onSubmit={handleSubmit}>
         <h2 className={style.title}></h2>
-
-        {psychologistForm ?
-          <div className={style.fieldBlock}>
-            <label className={style.subtitile}>Прізвище</label>
-            <Field name="lastName" component="input" className={style.field} />
-            <label className={style.subtitile}>Ім'я</label>
-            <Field name="firstName" component="input" className={style.field} />
-          </div> :
-          <div className={style.fieldBlock}>
-            <label className={style.subtitile}>Псевдонім</label>
-            <Field name="nickName" component="input" className={style.field} />
-          </div>
+        {
+          values.isPsychologist ?
+            <div>
+              <Field name="lastName" component="input" validate={composeValidators(required, minLength(2), maxLength(100))} >
+                {({ input, meta }) => (
+                  <div className={style.inputBlock}>
+                    <label className={style.subtitile}>{meta.error && meta.touched ? <span className={style.error}>{meta.error}</span> : "Прізвище"}</label>
+                    <input {...input} className={style.field} type="text" placeholder="Шевченко" />
+                  </div>
+                )}
+              </Field>
+              <Field name="firstName" validate={composeValidators(required, minLength(2), maxLength(100))} >
+                {({ input, meta }) => (
+                  <div className={style.inputBlock}>
+                    <label className={style.subtitile}>{meta.error && meta.touched ? <span className={style.error}>{meta.error}</span> : "Ім'я"}</label>
+                    <input {...input} className={style.field} type="text" placeholder="Олена" />
+                  </div>
+                )}
+              </Field>
+            </div> :
+            <Field name="nickName" validate={composeValidators(required, minLength(2), maxLength(100))} >
+              {({ input, meta }) => (
+                <div className={style.inputBlock}>
+                  <label className={style.subtitile}>{meta.error && meta.touched ? <span className={style.error}>{meta.error}</span> : "Псевдонім"}</label>
+                  <input {...input} className={style.field} type="text" placeholder="Anonym" />
+                </div>
+              )}
+            </Field>
         }
-
-        <label className={style.subtitile}>Електронна пошта</label>
-        <Field name="email" component="input" className={style.field} type="email" />
-        <label className={style.subtitile}>Пароль</label>
-        <Field name="password" component="input" className={style.field} type="password" />
-        <label className={style.subtitile}>Підтвердження паролю</label>
-        <Field name="rePassword" component="input" className={style.field} type="password" />
+        <Field name="email" validate={composeValidators(required, maxLength(200), emailPattern)}>
+          {({ input, meta }) => (
+            <div className={style.inputBlock}>
+              <label className={style.subtitile}>{meta.error && meta.touched ? <span className={style.error}>{meta.error}</span> : "Електронна пошта"}</label>
+              <input {...input} className={style.field} type="email" placeholder="email@address.com" />
+            </div>
+          )}
+        </Field>
+        <Field name="password" validate={composeValidators(required, minLength(8), maxLength(200), passwordPattern)} >
+          {({ input, meta }) => (
+            <div className={style.inputBlock}>
+              <label className={style.subtitile}>{meta.error && meta.touched ? <span className={style.error}>{meta.error}</span> : "Пароль"}</label>
+              <input {...input} className={style.field} type="password" placeholder="Створіть пароль (мінімум 8 символів)" />
+            </div>
+          )}
+        </Field>
+        <Field name="rePassword" validate={composeValidators(required, minLength(8), maxLength(200), passwordPattern, repeatPassword(values.rePassword))} >
+          {({ input, meta }) => (
+            <div className={style.inputBlock}>
+              <label className={style.subtitile}>{meta.error && meta.touched ? <span className={style.error}>{meta.error}</span> : "Підтвердження паролю"}</label>
+              <input {...input} className={style.field} type="password" placeholder="Повторіть пароль" />
+            </div>
+          )}
+        </Field>
         <div className={style.checkboxBlock}>
-          <Field name="isPsychologist" id="isPsychologist" component="input" type="checkbox" value={psychologistForm ? true : ""} onChange={onChangeForm} />
+          <Field name="isPsychologist" id="isPsychologist" component="input" type="checkbox" />
           <label htmlFor="isPsychologist" className={style.subtitile + ' ' + style.checkboxLabel}>Я психолог</label>
         </div>
         <Button>Зареєструватись</Button>
