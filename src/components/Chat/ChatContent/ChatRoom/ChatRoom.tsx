a627f6fce1987c8c07644b4e18bcaad4a72d6386
couldbefree <zoriaalexandr@gmail.com>
2021-05-03 19:41:48 +0300

    fixed url bug

--- src/components/Chat/ChatContent/ChatRoom/ChatRoom.tsx
+++ src/components/Chat/ChatContent/ChatRoom/ChatRoom.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useEffect } from 'react';
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import Respondents from "./Respondents/Respondents";
 import Messenger from "./Messenger/Messenger";
 import style from "./ChatRoom.module.css";
@@ -11,19 +11,19 @@ import { fetchUsers } from "../../../../redux/usersSlice";
 const ChatRoom: FunctionComponent = () => {
 
   const dispatch = useDispatch();
-  const history = useHistory();
+  // const history = useHistory();
   const userType = useSelector((state: RootState)  => state.authentication.currentUser?.userType);
   const users = useSelector((state: RootState) => state.users.users);
   const currentRespondent = useSelector((state: RootState) => state.respondent.currentRespondent);
 
-  
+
   useEffect(() => {
     userType && dispatch(fetchUsers(userType));
   }, []);
 
-  useEffect(() => {
+  /*useEffect(() => {
     users && history.push(`${history.location.pathname}/${users[0]._id}`)
-  }, [users]);
+  }, [users]);*/
 
   return (
     <div className={style.chatRoom}>
