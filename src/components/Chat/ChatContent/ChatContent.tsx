548fb688df6f6f3cc2fbdf884c4bed552ae57094
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-05-03 15:36:13 +0300

    corrected url

--- src/components/Chat/ChatContent/ChatContent.tsx
+++ src/components/Chat/ChatContent/ChatContent.tsx
@@ -8,7 +8,7 @@ import Users from "./Users/Users";
 const ChatContent: FunctionComponent = () => {
   return (
     <div className={style.chatContentBlock}>
-      <Route exact path="/chat" render={() => <ChatRoom />} />
+      <Route exact path="/chat/:userId?" render={() => <ChatRoom />} />
       <Route path="/chat/users" render={() => <Users />} />
       <Route path="/chat/profile/:userId?" render={() => <Profile />} />
     </div>
