3946645b61666a4f553079d9b22b1faee9b7d70d
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-05-04 19:39:47 +0300

    remove setTimeout

--- src/components/Chat/ChatContent/ChatRoom/Messenger/TopLiner/TopLiner.tsx
+++ src/components/Chat/ChatContent/ChatRoom/Messenger/TopLiner/TopLiner.tsx
@@ -10,19 +10,19 @@ const TopLiner: FunctionComponent = () => {
   const currentRespondent = useSelector((state: RootState) => state.respondent.currentRespondent)
   const status = useSelector((state: RootState) => state.users.usersStatus?.some(respondent => respondent.userId === currentRespondent?._id));
   const [typingStatus, setTypingStatus] = useState("");
-  let typingTimer: ReturnType<typeof setTimeout>;3
+  // let typingTimer: ReturnType<typeof setTimeout>;
 
 const onUserTyping = (userId: string): void => {
     console.log(currentRespondent?._id, '----', userId)
 
     if(currentRespondent?._id === userId) {
       console.log('currentRespondent is typing ..............', typingStatus)
-      clearTimeout(typingTimer);
+      // clearTimeout(typingTimer);
       setTypingStatus("typing...");
-      typingTimer = setTimeout(() => {
-        console.log('clear time out')
-        setTypingStatus("");
-      }, 1000);
+      // typingTimer = setTimeout(() => {
+      //   console.log('clear time out')
+      //   setTypingStatus("");
+      // }, 1000);
     }
   }
   
