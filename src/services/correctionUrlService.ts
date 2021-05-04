99654972283822b5e514c1d16c2405be24805656
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-05-04 21:28:40 +0300

    change base url

--- src/services/correctionUrlService.ts
+++ src/services/correctionUrlService.ts
@@ -2,7 +2,7 @@ import { CurrentUser } from "../types/currentUser";
 
 export const correctionUrl = (currentUser: CurrentUser): CurrentUser => {
   if (currentUser.photo) {
-    currentUser.photo = `https://server-psy-free.herokuapp.com//${currentUser.photo.replace("\\", "/")}`;
+    currentUser.photo = `https://server-psy-free.herokuapp.com/${currentUser.photo.replace("\\", "/")}`;
   }
   return currentUser;
 }
