604b5b4c995cef0a3b4f2d0a08163b56246c5189
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-05-06 21:34:17 +0300

    added interseptor

--- src/services/requestService.ts
+++ src/services/requestService.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { CurrentUser } from "../types/currentUser";
 import { MessageInterface } from "../types/messageInterface";
 import { PrimaryInfoFormData } from "../types/primaryInfoFormData";
@@ -7,39 +7,38 @@ import { persistanceService } from "./persistenceService";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: 'https://chat-server-app-node.herokuapp.com/api/v1/',
+  baseURL: 'http://localhost:5050/api/v1/',
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
+axios.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    const token = persistanceService.get('psy-free-token');
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
 export const authApi = {
   register: (name: string, password: string, email: string, userType: string): Promise<AxiosResponse> => {
-
     return instance.post("auth/register", {name, email, password, userType })
-      .then(response => {
-        axios.defaults.headers.common = {'Authorization': `Bearer ${response.data.token}`};
-        return response.data.token});
+      .then(response => response.data.token);
   },
 
   authMe: (): Promise<CurrentUser> => {
-    
 
-    const token = persistanceService.get('psy-free-token');
-
-    const config = {
-      headers: { Authorization: `Bearer ${token}` }
-    };
-
-    return instance.get("auth/me", config)
+    return instance.get("auth/me")
       .then(response => correctionUrl(response.data.data));
   },
 
   login: ( email: string,  password: string ): Promise<AxiosResponse> => {  
     return instance.post("auth/login", { password, email })
-      .then(response => {
-        axios.defaults.headers.common = {'Authorization': `Bearer ${response.data.token}`};
-        return response.data.token});
+      .then(response => response.data.token);
   },
 
   logout: (): Promise<AxiosResponse> => {  
@@ -54,8 +53,8 @@ export const profileApi = {
     return instance.put(`/users/${id}`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
       .then(response => correctionUrl(response.data.data));
   },
-  setInfo: (primaryInfo: PrimaryInfoFormData, id: string): Promise<CurrentUser> => {
 
+  setInfo: (primaryInfo: PrimaryInfoFormData, id: string): Promise<CurrentUser> => {
     return instance.put(`/users/${id}`, primaryInfo )
       .then(response => correctionUrl(response.data.data));
   }
@@ -63,33 +62,18 @@ export const profileApi = {
 
 export const usersApi = {
   getUsers: (userType: string): Promise<CurrentUser[]> => {
-
-    const token = persistanceService.get('psy-free-token');
-
-    const config = {
-      headers: { Authorization: `Bearer ${token}` }
-    };
-
     const queryParams: { [key: string]: string } = {
       'user': 'psychologist',
       'psychologist': 'user'
     }
-    
-    return instance.get(`users?user=${queryParams[userType]}`, config)
+    return instance.get(`users?user=${queryParams[userType]}`)
       .then(response => response.data.data.map((user: CurrentUser) => correctionUrl(user)));
   },
-  getMessages: (name: string): Promise<MessageInterface[]> => {
-    
-    const token = persistanceService.get('psy-free-token');
 
-    const config = {
-      headers: { Authorization: `Bearer ${token}` }
-    };
-    
+  getMessages: (name: string): Promise<MessageInterface[]> => {
     return instance.get("/users/messages", {
       params: {
-        user: name,
-        config
+        user: name
       }
     }).then(response => response.data.data);
   }
