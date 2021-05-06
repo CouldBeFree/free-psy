4a62f0043a0793d7abc306714ee81afc68bf5ef5
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-05-06 18:12:17 +0300

    changed base url

--- src/services/requestService.ts
+++ src/services/requestService.ts
@@ -6,7 +6,7 @@ import { correctionUrl } from "./correctionUrlService";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: 'https://chat-server-app-node.herokuapp.com/api/v1/api/v1/',
+  baseURL: 'https://chat-server-app-node.herokuapp.com/api/v1/',
   headers: {
     'Content-Type': 'application/json'
   }
--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,3 +1,3 @@
 import io from "socket.io-client";
 
-export const socket = io("https://chat-server-app-node.herokuapp.com/api/v1/");
+export const socket = io("https://chat-server-app-node.herokuapp.com/");
